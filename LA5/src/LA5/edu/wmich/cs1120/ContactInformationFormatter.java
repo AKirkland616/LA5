0fa7c4f428df1733d9d61470c41a4122ed1838ce
anthonykirkland <anthonykirkland@resnet198-076.wireless.wmich.edu>
2018-11-01 10:39:30 -0400

    Anthony Commit 2 Partial work on ContactInformationFormatter class

--- LA5/src/LA5/edu/wmich/cs1120/ContactInformationFormatter.java
+++ LA5/src/LA5/edu/wmich/cs1120/ContactInformationFormatter.java
@@ -1,11 +1,50 @@
 package LA5.edu.wmich.cs1120;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ContactInformationFormatter implements IContactInformationFormatter {
+Scanner reader;
+FormatExceptionHandler t = new FormatExceptionHandler();
+String nameA;
+String number;
+String emailA;
 
 	@Override
 	public void readContactInformation(String[] filePaths) {
 		// TODO Auto-generated method stub
 		
+		for (int i =0; i< filePaths.length; i++) {
+			File f = new File(filePaths[i]);
+			
+			try {
+				reader = new Scanner(f);
+			} catch (FileNotFoundException e) {
+				t.handleFileNotFoundException(e);
+			}
+			
+			try {
+				formatName(reader.nextLine());
+			} catch (NameFormatException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			try {
+				formatEmail(reader.nextLine());
+			} catch (EmailAddressFormatException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			try {
+				formatPhoneNumber(reader.nextLine());
+			} catch (PhoneNumberFormatException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
@@ -17,12 +56,27 @@ public class ContactInformationFormatter implements IContactInformationFormatter
 	@Override
 	public void formatEmail(String email) throws EmailAddressFormatException {
 		// TODO Auto-generated method stub
+		char [] e = email.toCharArray();
+		for(int i = 0; i < e.length; i++) {
+			if(e[i]== '.' || e[i] == '@') {
+				emailA += e[i];
+			}else {
+				if(Character.isLowerCase(e[i])) {
+					emailA += e[i];
+				}else if (!Character.isLowerCase(e[i])) {
+					emailA += Character.toLowerCase(e[i]);
+				}
+			}
+			 
+		}
 		
 	}
 
 	@Override
 	public void formatPhoneNumber(String phoneNumber) throws PhoneNumberFormatException {
 		// TODO Auto-generated method stub
+		char[] n = phoneNumber.toCharArray();
+		
 		
 	}
 
